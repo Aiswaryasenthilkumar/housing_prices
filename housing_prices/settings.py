400fff126f2f06717916a6d886fd1034a51d3a95
Aiswaryasenthilkumar <ishusenthil28042gmail.com>
2021-04-01 12:59:30 +0530

    edited

--- housing_prices/settings.py
+++ housing_prices/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '$-8!!sr$@%$owdl_9+cr*5!aa-l!m15i44ibh5ohca$y26(x3u'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['18.191.178.125','localhost']
+ALLOWED_HOSTS = ['18.220.178.226','localhost']
 
 
 # Application definition
